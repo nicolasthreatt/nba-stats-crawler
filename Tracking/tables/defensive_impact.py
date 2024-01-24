00d01b150c904a6bc3e1b62ec9b962b853547eab
Nicolas Threatt <44756925+nicolasthreatt@users.noreply.github.com>
2024-01-23 22:47:16 -0500

    Delete Tracking directory

--- Tracking/tables/defensive_impact.py
+++
@@ -1,8 +0,0 @@
-class DefensiveImpact:
-    def __init__(self):
-        self.stl             = float() # Steals
-        self.blk             = float() # Blocks
-        self.dreb            = float() # Defensive Rebounds
-        self.defended_fg_m   = float() # Field Goals Defended at Rim Made
-        self.defended_fg_a   = float() # Field Goals Defended at Rim Attempted
-        self.defended_fg_pct = float() # Field Goals Defended at Rim Percent
--- Tracking/tables/speed_distance.py
+++
@@ -1,9 +0,0 @@
-class SpeedDistance:
-    def __init__(self):
-        self.dist_feet      = float() # Distance Feet
-        self.dist_miles     = float() # Distance Miles
-        self.dist_miles_off = float() # Distance Miles Offense
-        self.dist_miles_def = float() # Distance Miles Defense
-        self.avg_speed      = float() # Average Speed
-        self.avg_speed_off  = float() # Average Speed Offense
-        self.avg_speed_def  = float() # Average Speed Defense
